2bb8933ba89a446d6c890427167ddce4d2aca4e5
Jesus Suarez <jsuarez@PORTATIL1188-PC.birchman.local>
2022-06-06 14:11:54 +0200

    First Commit

---
+++ main.tf
@@ -0,0 +1,124 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.9.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "f801f268-4bbc-4ba4-8ee5-c6447296eaef"
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "iac-demo-rg"
+  location = "West Europe"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "iac-demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet1" {
+  name                 = "iac-internal"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "publicip1" {
+  name                = "iac-PublicIP"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "iac-nsg"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic1" {
+  name                = "iac-nic"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip1.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "association-nsg1" {
+  network_interface_id      = azurerm_network_interface.nic1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "iac-vm"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+  size                = "Standard_D2as_v5"
+  computer_name       = "iac-worker"
+  admin_username      = "adminuser"
+  disable_password_authentication = true
+  network_interface_ids = [
+    azurerm_network_interface.nic1.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("./ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg1.name
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.vm1.public_ip_address
+}
+
